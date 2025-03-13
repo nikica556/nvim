77275e54b75c329245fd00be21fd1ca913926640
Nik Korošec <nikkorosec@Niks-MacBook-Pro.local>
2025-03-13 11:20:25 +0100

    removed diffview

--- lua/core/plugins/diffview.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "sindrets/diffview.nvim",
-
-  config = function() -- This is the function that runs, AFTER loading
-    vim.keymap.set("n", "<leader>dv", function()
-      vim.cmd.DiffviewOpen() -- Toggle the DiffView
-    end, { desc = "Toggle [D]iff[V]iew" })
-  end,
-}
