7a5063383246737f0ec2a0b07e62ec240fc0814c
Nik Korošec <nikkorosec@Niks-MacBook-Pro.local>
2025-04-18 10:22:17 +0200

    added basic copilot keybinds

--- lua/core/plugins/copilot.lua
+++ lua/core/plugins/copilot.lua
@@ -6,5 +6,21 @@ return {
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     opts = {},
+    keys = {
+      {
+        "<leader>cc",
+        function()
+          vim.api.nvim_command("CopilotChat")
+        end,
+        desc = "[C]opilot [C]hat",
+      },
+      {
+        "<leader>cm",
+        function()
+          vim.api.nvim_command("CopilotChatModels")
+        end,
+        desc = "[C]opilot [M]odels",
+      },
+    },
   },
 }
