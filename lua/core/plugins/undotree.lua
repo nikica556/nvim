0d9bfc10f0b6101e09cc6c2fae29d33a49638975
Nik Korošec <nikkorosec@Niks-MacBook-Pro.local>
2025-04-19 18:41:33 +0200

    refactor(undotree): use keys and opts for lazy loading
    Replace manual config function with the lazy.nvim's keys and opts
    configuration structure for better lazy loading behavior and more
    consistent plugin setup pattern.

--- lua/core/plugins/undotree.lua
+++ lua/core/plugins/undotree.lua
@@ -1,8 +1,13 @@
 return {
   "mbbill/undotree",
-  config = function() -- This is the function that runs, AFTER loading
-    vim.keymap.set("n", "<leader>u", function()
-      vim.cmd.UndotreeToggle() -- Toggle the Undotree
-    end, { desc = "Toggle [U]ndotree" })
-  end,
+  opts = {},
+  keys = {
+    {
+      "<leader>u",
+      function()
+        vim.cmd.UndotreeToggle() -- Toggle the Undotree
+      end,
+      desc = "Toggle [U]ndotree",
+    },
+  },
 }
